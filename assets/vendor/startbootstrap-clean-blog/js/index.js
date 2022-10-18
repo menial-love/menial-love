57f6e434caf447173bf85b341177f29d2e4d4dc1
menial-love <bowenwwang@163.com>
2022-10-18 14:09:59 +0800

    Update index.js

--- assets/vendor/startbootstrap-clean-blog/js/index.js
+++ assets/vendor/startbootstrap-clean-blog/js/index.js
@@ -1,8 +1,13 @@
 let svg = document.getElementsByClassName('iconfont')[0];
 svg.addEventListener("mouseover", function (event) {
+    console.log("mouseover");
     svg.style.color = '#4775f1';
     svg.style.fontWeight = 'bold';
 }, false);
 svg.addEventListener("mouseout", function (event) {
+    console.log("mouseout");
     svg.removeAttribute('style');
 }, false);
+svg.addEventListener("click", function (envent) {
+    window.open('https://github.com/Lx-958/Multi-agent-System');
+});
