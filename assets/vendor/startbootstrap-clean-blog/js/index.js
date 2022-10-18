bb128c7af1485446e4184ae4e5ac74d2e1cb00c1
menial-love <bowenwwang@163.com>
2022-10-18 14:31:53 +0800

    Update index.js

--- assets/vendor/startbootstrap-clean-blog/js/index.js
+++ assets/vendor/startbootstrap-clean-blog/js/index.js
@@ -1,12 +1,9 @@
 let svg = document.getElementsByClassName('iconfont')[0];
-console.log(svg);
 svg.addEventListener("mouseover", function (event) {
-    console.log("mouseover");
     svg.style.color = '#4775f1';
     svg.style.fontWeight = 'bold';
 }, false);
 svg.addEventListener("mouseout", function (event) {
-    console.log("mouseout");
     svg.removeAttribute('style');
 }, false);
 svg.addEventListener("click", function (envent) {
