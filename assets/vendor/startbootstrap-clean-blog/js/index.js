3d12e7fa720f8ff7294d5666cd53edf9569b420b
menial-love <bowenwwang@163.com>
2022-10-19 09:29:18 +0800

    update
    update

--- assets/vendor/startbootstrap-clean-blog/js/index.js
+++ assets/vendor/startbootstrap-clean-blog/js/index.js
@@ -1,6 +1,6 @@
 let svg = document.getElementsByClassName('iconfont')[0];
 svg.addEventListener("mouseover", function (event) {
-    svg.style.color = '#4775f1';
+    svg.style.color = '#d32f2f';
     svg.style.fontWeight = 'bold';
 }, false);
 svg.addEventListener("mouseout", function (event) {
